7c435166679612e6731f1b08e6d8a2bd29c68d44
Christofer Nyström <nystromchristofer@gmail.com>
2025-07-13 18:35:18 +0200

    Initial commit with tray icon

---
+++ WinFlare/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+using System.Drawing;
+using Forms = System.Windows.Forms;
+
+namespace WinFlare
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : System.Windows.Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            var notifyIcon = new Forms.NotifyIcon
+            {
+                Icon = new Icon("Resources/icon.ico"),
+                Visible = true,
+                Text = "WinFlare"
+            };
+
+
+        }
+    }
+}

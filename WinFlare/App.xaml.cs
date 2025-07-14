e119ab66ef792d75d5834c85f7a04806eb654da6
Christofer Nyström <nystromchristofer@gmail.com>
2025-07-14 23:29:28 +0200

    fixes minimizing behavior

--- WinFlare/App.xaml.cs
+++ WinFlare/App.xaml.cs
@@ -1,27 +1,40 @@
-﻿using System.Configuration;
-using System.Data;
-using System.Windows;
-using System.Drawing;
+﻿using System.Windows;
 using Forms = System.Windows.Forms;
 
 namespace WinFlare
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : System.Windows.Application
     {
         protected override void OnStartup(StartupEventArgs e)
         {
             base.OnStartup(e);
+            var mainWindow = new MainWindow();
+            mainWindow.Show(); 
+            mainWindow.Activate(); 
+
+            mainWindow.StateChanged += (s, args) =>
+            {
+                if (mainWindow.WindowState == WindowState.Minimized)
+                {
+                    mainWindow.Hide();
+                }
+            };
 
             var notifyIcon = new Forms.NotifyIcon
             {
                 Icon = new Icon("Resources/icon.ico"),
                 Visible = true,
-                Text = "WinFlare"
+                Text = "WinFlare",
             };
 
+            notifyIcon.Click += (sender, args) =>
+            {
+                mainWindow.Show();
+                mainWindow.WindowState = WindowState.Normal;
+                mainWindow.Topmost = true;
+                mainWindow.Activate();
+                mainWindow.Topmost = false;
+            };
 
         }
     }

317f423367665c7928339fc070f88dad0ecd118b
Christofer Nyström <nystromchristofer@gmail.com>
2025-07-16 01:13:47 +0200

    adds code to loop through drivers

--- WinFlare/Casher.cs
+++ WinFlare/Casher.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,7 +15,21 @@ namespace WinFlare
             cash.Add("C:");
             cash.Add("D:");
 
+
+            foreach (DriveInfo drive in DriveInfo.GetDrives())
+            {
+                if (drive.IsReady && drive.DriveType == DriveType.Fixed)
+                {
+                    cash.Add(drive.Name);
+                }
+            }
+
             return cash;
+
+
+
+
+
         }
 
     }
